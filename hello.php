3eb1abccc37bea6a8806e57a3de956760941596b
EnzoDiass <enzohest@gmail.com>
2025-01-21 14:43:43 -0300

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,225 @@
+
+
+
+
+<!-- <form>
+
+    <input type="text" name="nome">
+    <input type="date" name="nascimento">
+    <input type="submit" value="ok">
+
+</form> -->
+
+<?php 
+
+// $a = 12;
+// $b = 89;
+// $c = "teste";
+
+
+// if(is_int($a)|| is_float($a)) {
+
+
+
+//     $multi1 = $a * 2;
+
+//     if($multi1 > 100) {
+
+//         echo "o numero é maior que 100<br>";
+
+
+
+//     } else {
+
+//         echo "o numero não é maior que 100";
+//     }
+
+
+
+// }else{
+
+//     echo "não é um numero ";
+// }
+
+
+
+// if(is_int($b)|| is_float($b)) {
+
+
+
+//     $multi1 = $b * 2;
+
+//     if($multi1 > 100) {
+
+//         echo "o numero é maior que 100<br>";
+
+
+
+//     } else {
+
+//         echo "o numero não é maior que 100";
+//     }
+
+
+
+// }else{
+
+//     echo "não é um numero ";
+// }
+
+
+
+
+// if(is_int($c)|| is_float($c)) {
+
+
+
+//     $multi1 = $c * 2;
+
+//     if($multi1 > 100) {
+
+//         echo "o numero é maior que 100<br>";
+
+
+
+//     } else {
+
+//         echo "o numero não é maior que 100";
+//     }
+
+
+
+// }else{
+
+//     echo "não é um numero ";
+// }
+
+
+
+// if(10 > 5){
+
+//     echo"Entrou no if <br>";
+
+
+// } else if (10 > 6){
+
+
+//     echo "Entrou no else if <br>";
+
+// }
+
+
+// if(10 < 5){
+
+//     echo"Entrou no if 2 <br>";
+
+
+// } else if (10 > 6){
+
+
+//     echo "Entrou no else if 2 <br>";
+    
+// } else {
+
+//     echo "entrou no else 3";
+// }
+
+
+// $num = 0;
+
+// while ($num < 5){
+//     echo "menor que cinco   <br> ";
+
+//     $num = $num +1;
+
+//     $teste = "teste";
+
+// }
+
+// echo $num; 
+
+// echo $teste;
+
+
+
+// for($i = 0; $i <= 5; $i++ ){
+
+//     if($i == 5){
+
+//         echo "aqui é o valor de  $i <br> ";
+//     }
+
+//     echo "é menor que 10 <br>";
+// }  
+
+
+
+
+// $idade = 15;
+// $idade = 16;
+// $idade = 17;
+
+
+// $maioridade = 18;
+
+
+// if($idade >= $maioridade){
+
+//     echo "Voce é maior de idade <br>";
+// }
+
+
+// $d = 15;
+// $f = 30.87;
+
+// $g = $d + $f ;
+
+// echo $g;
+
+
+
+// $a= 10;
+
+// $b = "teste";
+// $c = 12.8;
+
+// if(is_float($b)){
+//     echo "è float ! <br>";
+
+// }
+
+// if (is_float($c)){
+//     echo "é float 2! ";
+// }
+
+
+
+// $a= 10;
+
+// if (is_int($a)){
+//     echo "è um numero inteiro";
+// }
+
+
+// $meses = array (
+//     "Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"
+
+// );
+
+
+
+
+// $condicao = true;
+
+// while ($condicao) {
+
+//     $numero = rand(1,10);
+
+//     if($numero === 3) {
+
+//         echo "TRES !!!";
+//         $condicao = false;
+
+//     }
+//     echo $numero . "";
+// }
